49eaa6c1a337f2880c1719893de4a99ffc016a3f
kingsley.hendrickse <kingsley.hendrickse@patchwork.health>
2024-08-04 18:50:15 +0100

    specs

--- tests/integration/integration_test.rs
+++ tests/integration/integration_test.rs
@@ -1,39 +1,30 @@
-// use pg_filters::{filtering::{FilteringRule, ColumnName}, sorting::{SortedColumn}, PaginationOptions, PgFilters};
-// use pg_filters::FilteringOptions;
-// use testcontainers_modules::{postgres, testcontainers::runners::SyncRunner};
-// use testcontainers_modules::postgres::Postgres;
-// #[test]
-// fn test_something_works() {
-//
-//     let container = postgres::Postgres::default().start().unwrap();
-//
-//
-//     // prepare connection string
-//     // let connection_string = &format!(
-//     //     "postgres://postgres:postgres@127.0.0.1:{}/postgres",
-//     //     container.get_host_port_ipv4(5432)?
-//     // );
-//     // container is up, you can use it
-//     // let mut conn = postgres::Client::connect(connection_string, postgres::NoTls).unwrap();
-//     // let rows = conn.query("SELECT 1 + 1", &[]).unwrap();
-//     // assert_eq!(rows.len(), 1);
-//
-//     let filters = PgFilters::new(
-//        Some(PaginationOptions {
-//            current_page: 1,
-//            per_page: 10,
-//            per_page_limit: 10,
-//            total_records: 1000,
-//        }),
-//        vec![
-//            SortedColumn::new("age", "desc"),
-//            SortedColumn::new("name", "asc"),
-//        ],
-//        Some(FilteringOptions::new(vec![
-//            FilteringRule::new("where", ColumnName::String("name"), "=", "John"),
-//            FilteringRule::new("or", ColumnName::Int("age"), ">", "18"),
-//        ])),
-//     );
-//
-//     println!("{:?}", filters.sql());
-// }
+use crate::integration::client;
+
+#[tokio::test]
+async fn test_something_works() {
+    let client = client().await;
+
+    let rows = client
+        .query("SELECT $1::TEXT", &[&"hello world"])
+        .await.unwrap();
+
+    // And then check that we got back the same string we sent over.
+    let value: &str = rows[0].get(0);
+    assert_eq!(value, "hello world");
+}
+
+
+
+#[tokio::test]
+async fn test_something_works2() {
+    let client = client().await;
+
+    let rows = client
+        .query("SELECT $1::TEXT", &[&"hello world"])
+        .await.unwrap();
+
+    // And then check that we got back the same string we sent over.
+    let value: &str = rows[0].get(0);
+    assert_eq!(value, "hello world");
+}
+
--- tests/integration/mod.rs
+++ tests/integration/mod.rs
@@ -1,1 +1,36 @@
+use testcontainers_modules::postgres::Postgres;
+use testcontainers_modules::testcontainers::ContainerAsync;
+use testcontainers_modules::testcontainers::core::error;
+use testcontainers_modules::testcontainers::runners::AsyncRunner;
+use tokio::sync::OnceCell;
+use tokio_postgres::{Client, NoTls};
+
 pub mod integration_test;
+
+pub async fn client() -> Client {
+    get_client(get_connection_string().await).await
+}
+
+async fn get_connection_string() -> String {
+    let container = get_container().await.as_ref().unwrap();
+    let port = container.get_host_port_ipv4(5432).await.unwrap();
+    let host = container.get_host().await.unwrap();
+    format!("host={} user=postgres password=postgres port={}", host, port)
+}
+
+async fn get_client(connection_string: String) -> Client {
+    let (client, connection) =
+        tokio_postgres::connect(connection_string.as_str(), NoTls).await.unwrap();
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+    client
+}
+
+pub async fn get_container() -> &'static error::Result<ContainerAsync<Postgres>> {
+    static ONCE: OnceCell<error::Result<ContainerAsync<Postgres>>> = OnceCell::const_new();
+    ONCE.get_or_init(|| async { Postgres::default().start().await }).await
+}
